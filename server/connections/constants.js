4e6c0035fed52b8946b87883fec9c766406ce5bb
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-11-30 00:34:48 +0530

    Feat:Uploading added

--- server/connections/constants.js
+++ server/connections/constants.js
@@ -1,11 +1,6 @@
 
 const constants = {
     PORT: process.env.PORT || 3000,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-    recaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
-    DATABASE_KEY: process.env.DATABASE_KEY,
-    CRYPTO_SECRET_KEY: process.env.SECRET_KEY,
-    CACHE_TTL: 7200000,
     WELLBORE:{
         "header":1,
         "data":2
