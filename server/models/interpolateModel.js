01190695215720a4e4dbbd147b4949017fccf386
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-18 00:15:39 +0530

    Comle

--- server/models/interpolateModel.js
+++ server/models/interpolateModel.js
@@ -4,13 +4,13 @@ const interpolateSchema = new mongoose.Schema(
     {
         excelName: { type: String, trim: true, required: true },
         userId: { type: String, trim: true, required: true },
-        md: { type: Number, trim: true, required: true },
-        tvd: { type: Number, trim: true, required: true },
-        ew: { type: Number, trim: true, required: true },
-        ns: { type: Number, trim: true, required: true },
-        rf: { type: Number, trim: true, required: true },
-        inc: { type: Number, trim: true, required: true },
-        azi: { type: Number, trim: true, required: true }
+        md: { type: String, trim: true, required: true },
+        tvd: { type: String, trim: true, required: true },
+        ew: { type: String, trim: true, required: true },
+        ns: { type: String, trim: true, required: true },
+        rf: { type: String, trim: true, required: true },
+        inc: { type: String, trim: true, required: true },
+        azi: { type: String, trim: true, required: true }
     },
     { versionKey: false }
 );
