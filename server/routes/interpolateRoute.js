ade43028009fc6d2e599230a8431c2d839e6fec4
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-18 18:42:57 +0530

    Complete

--- server/routes/interpolateRoute.js
+++ server/routes/interpolateRoute.js
@@ -1,6 +1,6 @@
-const {interpolateController} = require("../controllers/interpolateController");
+const {interpolateController, getInterpolate} = require("../controllers/interpolateController");
 const router = require("express").Router();
 
 router.post("/interpolate",interpolateController);
-router.get("/getInterpolate",interpolateController);
+router.get("/getInterpolate",getInterpolate);
 module.exports = router;
