b0cb3baae2ca12fc11c30d0c0428c76a40e6c786
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-18 14:06:43 +0530

    Completed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,6 @@ const { register, redirect } = require("../controllers/authController");
 const limiter = require("../middleware/limiter");
 const router = require("express").Router();
 
-router.get("/register", limiter, register);
+router.get("/register", register);
 router.get("/:id", redirect);
 module.exports = router;
