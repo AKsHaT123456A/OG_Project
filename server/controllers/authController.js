c0629033d814349662dc10644437971c76334403
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-18 13:55:49 +0530

    Completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,39 +1,30 @@
 const User = require("../models/user");
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
+
 const register = async (req, res) => {
     try {
         const id = uuidv4();
         const userId = req.cookies.id;
         const user = await User.findOne({ id: userId });
+
         if (user) {
-            if (!userId) {
-                return res.redirect(`https://temp-project-alpha.vercel.app/404`);
-            }
             return res.redirect(`https://og-project.onrender.com/api/v1/auth/${userId}`);
         }
-        await User.create({
-            id
-        });
 
-        return res.redirect(`https://og-project.onrender.com/api/v1/auth/${id}`)
+        await User.create({ id });
+        return res.redirect(`https://og-project.onrender.com/api/v1/auth/${id}`);
     } catch (err) {
-        res
-            .status(500)
-            .json({ message: "Registration failed", error: err.message });
+        res.status(500).json({ message: "Registration failed", error: err.message });
     }
 };
 
 const redirect = (req, res) => {
-    if (!req.cookies.id) {
-        console.log({ message: "No cookie found" });
-        const fifteenDays = 15 * 24 * 60 * 60 * 1000;
-        res.cookie('id', req.params.id, { maxAge: fifteenDays, httpOnly: true });
-    }
+    // Always set the cookie, regardless of whether it exists
+    const fifteenDays = 15 * 24 * 60 * 60 * 1000;
+    res.cookie('id', req.params.id, { maxAge: fifteenDays, httpOnly: true });
+
     console.log({ message: "Cookie found", cookie: req.cookies.id });
-    return res.redirect(`https://temp-project-alpha.vercel.app/`)
-}
-
-
+    return res.redirect(`https://temp-project-alpha.vercel.app/`);
+};
 
 module.exports = { register, redirect };
-//setup ,remove created 
