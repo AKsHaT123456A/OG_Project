2a20a724ef2075ce2a74399cb4424bd6e45e2ef9
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-18 14:43:12 +0530

    Completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,46 +1,40 @@
 const User = require("../models/user");
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require('uuid')
 const register = async (req, res) => {
     try {
         const id = uuidv4();
-        const userId = req.headers['user-id'];
+        const userId = req.cookies.userId;
         const user = await User.findOne({ id: userId });
-
-        console.log({ user, userId });
-
+        console.log({ user , userId });
         if (user) {
             if (!userId) {
                 return res.redirect(`https://temp-project-alpha.vercel.app/404`);
             }
             return res.redirect(`https://og-project.onrender.com/api/v1/auth/${userId}`);
         }
-
         await User.create({
             id
         });
 
-        // Set 'user-id' header in the response
-        res.set('user-id', id);
-        return res.redirect(`https://og-project.onrender.com/api/v1/auth/${id}`);
+        return res.redirect(`https://og-project.onrender.com/api/v1/auth/${id}`)
     } catch (err) {
-        res.status(500).json({ message: "Registration failed", error: err.message });
+        res
+            .status(500)
+            .json({ message: "Registration failed", error: err.message });
     }
 };
 
-const redirect = async (req, res) => {
-    const userId = req.headers['user-id']; // Use 'user-id' header
-
-    if (!userId) {
-        console.log({ message: "No header found" });
+const redirect = (req, res) => {
+    if (!req.cookies.userId) {
+        console.log({ message: "No cookie found" });
         const fifteenDays = 15 * 24 * 60 * 60 * 1000;
-
-        // Set 'user-id' header in the response
-        await res.set('user-id', req.params.id);
+        res.cookie('userId', req.params.id, { maxAge: fifteenDays, httpOnly: true });
     }
+    console.log({ message: "Cookie found", cookie: req.cookies.userId });
+    return res.redirect(`https://temp-project-alpha.vercel.app/`)
+}
 
-    console.log({ message: "Header found", header: userId });
-    return res.redirect(`https://temp-project-alpha.vercel.app/`);
-};
+
 
 module.exports = { register, redirect };
+//setup ,remove created 
