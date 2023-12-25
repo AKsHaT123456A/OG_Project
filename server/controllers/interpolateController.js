354611b213ce679f35435888e109e3cbc41e91c8
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-25 18:09:53 +0530

    Completed

--- server/controllers/interpolateController.js
+++ server/controllers/interpolateController.js
@@ -19,27 +19,30 @@ const interpolateController = async (req, res) => {
         }
 
         const [station1, station2] = await Promise.all([
-            WellPlannedExcelModel.findOne({ md: { $lte: md }, excelName ,userId:id})
+            WellPlannedExcelModel.findOne({ md: { $lte: md }, excelName })
                 .sort({ md: -1 })
                 .limit(1)
                 .select('md inc azi tvd east north -_id'),
-            WellPlannedExcelModel.findOne({ md: { $gte: md }, excelName,userId:id })
+            WellPlannedExcelModel.findOne({ md: { $gte: md }, excelName })
                 .sort({ md: 1 })
                 .limit(1)
                 .select('md dls buildrate turnrate -_id')
         ]);
+
         const brX = parseFloat(station2.buildrate) / 100;
         const trX = parseFloat(station2.turnrate) / 100;
 
-        const inc1 = (station1.inc + brX * (md - station1.md));
+        const incX = station1.inc + brX * (md - station1.md);
         const aziX = station1.azi + trX * (md - station1.md);
-        const dlX = station2.dls * (station2.md - station1.md);
+        const dl = (station2.dls * (station1.md - station2.md)) / 100;
+        const dlX = dl * (md - station1.md) / (station2.md - station1.md);
         const rf = calculateRF(dlX);
-        const incX = inc1.toFixed(2);
+
         const deltaTVD = calculateDeltaTVD(station1.inc, incX, rf, md - station1.md);
         const deltaEW = calculateDeltaEW(station1.inc, incX, station1.azi, aziX, rf, md - station1.md);
         const deltaNS = calculateDeltaNS(station1.inc, incX, station1.azi, aziX, rf, md - station1.md);
-        const tvd = parseFloat((deltaTVD + station1.tvd));
+
+        const tvd = parseFloat((deltaTVD + station1.tvd).toFixed(2));
         const ew = parseFloat((deltaEW + station1.east).toFixed(2));
         const ns = parseFloat((deltaNS + station1.north).toFixed(2));
 
@@ -48,7 +51,7 @@ const interpolateController = async (req, res) => {
             tvd,
             ew,
             ns,
-            inc: parseFloat(incX),
+            inc: parseFloat(incX.toFixed(2)),
             azi: parseFloat(aziX.toFixed(2)),
             rf,
             excelName,
