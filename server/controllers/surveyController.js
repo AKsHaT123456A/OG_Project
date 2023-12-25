ed1f94efe52a572cf584436f4c7311601091618f
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-12-25 22:07:57 +0530

    Completed

--- server/controllers/surveyController.js
+++ server/controllers/surveyController.js
@@ -52,9 +52,9 @@ const lengthSurvey = async (req, res) => {
     try {
         const { id } = req.query;
         const { logName } = req.query;
-        const surveys = await survey.find({ userId: id, logName }).sort({ fieldNumber: -1 });
-        const fieldNumber = surveys[0].fieldNumber;
-        return res.status(200).json({ length: fieldNumber });
+        const surveys = await survey.find({ userId: id, logName });
+        const length = surveys.length;
+        return res.status(200).json({ length });
     } catch (err) {
         return res.status(500).json({ err: err.message });
     }
